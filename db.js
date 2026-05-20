fcf8bfa2003b2ab068e2f219514fa75beaaa410d
AmshudharReddy <pra.213178882@gmail.com>
2026-05-20 18:12:41 +0530

    Fixed mongodb connection

--- db.js
+++ db.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
-const mongoURL = process.env.MONGO_URI || "mongodb://localhost:27017/TRAVIS";
 
-const connectToMongo = async () =>{
-    await mongoose.connect(mongoURL);
-    console.log("Connected to MongoDB successfully.");
-}
+const connectToMongo = async () => {
+    try {
+        const mongoURL = process.env.MONGO_URI;
+
+        if (!mongoURL) {
+            throw new Error("MONGO_URI environment variable is missing.");
+        }
+
+        await mongoose.connect(mongoURL);
+
+        console.log("Connected to MongoDB successfully.");
+    } catch (error) {
+        console.error("MongoDB Connection Error:");
+        console.error(error);
+
+        process.exit(1);
+    }
+};
 
 module.exports = connectToMongo;
